ef192a07c641b181fa850f570c744f578abb7c50
iPoon <pawares.r@gmail.com>
2018-02-10 11:37:58 +0700

    Add Create User activity UI

---
+++ app/src/main/java/com/ipoondev/android/psugo/login/CreateUserActivity.kt
@@ -0,0 +1,17 @@
+package com.ipoondev.android.psugo.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ipoondev.android.psugo.R
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+
+
+    }
+
+
+}
--- app/src/main/java/com/ipoondev/android/psugo/login/LoginActivity.kt
+++ app/src/main/java/com/ipoondev/android/psugo/login/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.ipoondev.android.psugo.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ipoondev.android.psugo.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button_login_create_user.setOnClickListener {
+            val createUserIntent =  Intent(this, CreateUserActivity::class.java)
+            startActivity(createUserIntent)
+        }
     }
+
 }

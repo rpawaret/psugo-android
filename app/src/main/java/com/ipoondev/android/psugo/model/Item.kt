eec2ceeef6154b79ad58829afc81c8533ef8fd50
iPoon <pawares.r@gmail.com>
2018-02-11 22:58:07 +0700

    display missions collection by implement FirestoreRecyclerAdapter

--- app/src/main/java/com/ipoondev/android/psugo/model/Item.kt
+++ app/src/main/java/com/ipoondev/android/psugo/model/Item.kt
@@ -2,4 +2,22 @@ package com.ipoondev.android.psugo.model
 
 import com.google.firebase.firestore.GeoPoint
 
-class Item(val id: Int, val title: String, val address: String, val geoPoint: GeoPoint, val timeout: Long, val radius: Float)
+class Item {
+    var id: Int = 0
+    var title: String? = null
+    var address: String? = null
+    var geoPoint: GeoPoint? = null
+    var timeout: Long? = null
+    var radius: Float? = null
+
+    constructor() {}
+
+    constructor(id: Int, title: String, address: String, geoPoint: GeoPoint, timeOut: Long?, radius: Float?) {
+        this.id = id
+        this.title = title
+        this.address = address
+        this.geoPoint = geoPoint
+        this.timeout = timeOut
+        this.radius = radius
+    }
+}
--- app/src/main/java/com/ipoondev/android/psugo/model/Mission.kt
+++ app/src/main/java/com/ipoondev/android/psugo/model/Mission.kt
@@ -1,5 +1,21 @@
 package com.ipoondev.android.psugo.model
 
-import java.util.*
 
-class Mission(val id: Int, val title: String, val image: String, val teacher: String, val items: List<Item>)
+class Mission {
+    var id: Int = 0
+    var title: String? = null
+    var image: String? = null
+    var teacher: String? = null
+    var items: List<Item>? = emptyList()
+
+    constructor() {}
+
+    constructor(id: Int, title: String, image: String, teacher: String, items: List<Item>) {
+        this.id = id
+        this.title = title
+        this.image = image
+        this.teacher = teacher
+        this.items = items
+    }
+}
+

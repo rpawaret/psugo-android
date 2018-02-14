e911629f255d6b28cc3c7c52aad503a95975c926
iPoon <pawares.r@gmail.com>
2018-02-14 18:55:15 +0700

    implement pause mission function

--- app/src/main/java/com/ipoondev/android/psugo/mission/MissionDetailActivity.kt
+++ app/src/main/java/com/ipoondev/android/psugo/mission/MissionDetailActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.ipoondev.android.psugo.R
-import com.ipoondev.android.psugo.geofencing.Geofencing
 import com.ipoondev.android.psugo.model.Item
 import com.ipoondev.android.psugo.model.Mission
 import com.ipoondev.android.psugo.model.MyMission
@@ -16,17 +15,16 @@ import java.util.*
 
 class MissionDetailActivity : AppCompatActivity() {
     val TAG = MissionDetailActivity::class.simpleName
-    var isStart = false
-    private var geofencing: Geofencing? = null
+    var isPlaying = false
     lateinit var mFirestore: FirebaseFirestore
     lateinit var missionId: String
+    lateinit var playerId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mission_details)
         setSupportActionBar(toolbar_mission_detail)
-
-        geofencing = Geofencing(this)
+        playerId = FirebaseAuth.getInstance().currentUser!!.uid
 
         missionId = intent.extras.getString(EXTRA_MISSION_ID)
                 ?: throw IllegalArgumentException("Must pass extra $EXTRA_MISSION_ID")
@@ -34,15 +32,15 @@ class MissionDetailActivity : AppCompatActivity() {
         mFirestore = FirebaseFirestore.getInstance()
 
         val missionRef = mFirestore.collection("missions").document(missionId)
+        missionRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    val mission = documentSnapshot.toObject(Mission::class.java)
+                    toolbar_mission_detail.title = mission.name
+                    text_mission_detail_detail.text = mission.detail
+                    text_mission_detail_subject.text = "Subject: ${mission.subject}"
+                    text_mission_detail_owner.text = "Teacher: ${mission.ownerName}"
 
-        missionRef.get().addOnSuccessListener { documentSnapshot ->
-            val mission = documentSnapshot.toObject(Mission::class.java)
-            toolbar_mission_detail.title = mission.name
-            text_mission_detail_detail.text = mission.detail
-            text_mission_detail_subject.text = "Subject: ${mission.subject}"
-            text_mission_detail_owner.text = "Teacher: ${mission.ownerName}"
-
-        }.addOnFailureListener { exception ->
+                }.addOnFailureListener { exception ->
                     Log.e(TAG, exception.localizedMessage)
                 }
 
@@ -62,53 +60,85 @@ class MissionDetailActivity : AppCompatActivity() {
             }
         }
 
-        button_start.setOnClickListener {
+        val myMissionRef = mFirestore
+                .collection("players").document(playerId)
+                .collection("myMissions").document(missionId)
+        myMissionRef.get()
+                .addOnCompleteListener {
+                    checkNotNull(it).apply {
+                        Log.d(TAG, "DocumentSnapshot data: " + this.result.data)
+                        val myMission = this.result.toObject(MyMission::class.java)
+                        button_play.text = myMission.state
+                    }
+                }.addOnFailureListener { e ->
+                    Log.d(TAG, "Error getting Document: ${e.localizedMessage}")
 
-            if (!isStart) {
-                startMission()
-                button_start.text = "Started"
+                }
+
+
+        button_play.setOnClickListener {
+
+            if (!isPlaying) {
+                playMission()
             } else {
-                stopMission()
-                button_start.text = "start"
+                pauseMission()
             }
         }
 
     }
 
-    fun startMission() {
-        isStart = true
+    fun playMission() {
 
-        val playerId = FirebaseAuth.getInstance().currentUser!!.uid
         val batch = FirebaseFirestore.getInstance().batch()
 
-        // TODO set the new document in myMissions subcollection
+        // set the new document in myMissions subcollection
         val myMissionsRef = FirebaseFirestore.getInstance()
                 .collection("players").document(playerId)
                 .collection("myMissions").document(missionId)
         batch.set(myMissionsRef, MyMission(missionId, 0, "Playing"))
 
-        // TODO update the currentMissionId field in playerId document
+        // update the currentMissionId field in playerId document to missionId
         val playerIdRef = FirebaseFirestore.getInstance()
                 .collection("players").document(playerId)
-
         batch.update(playerIdRef, "currentMissionId", missionId)
 
-        // TODO commit
+        // commit
         batch.commit()
                 .addOnCompleteListener {
-                    Log.d(TAG, "Add myMission Document and update currentMissionId successful")
+                    Log.d(TAG, "ADD myMission Document and UPDATE currentMissionId successful")
+                    // set button.text = playing when user playing
+                    isPlaying = true
+                    button_play.text = "Playing"
                 }.addOnFailureListener {
                     Log.d(TAG, "ADD and UPDATE failed")
                 }
     }
 
-    fun stopMission() {
-        isStart = false
+    fun pauseMission() {
 
-        // TODO unregister geofences
-//        GeofencingService.unRegisterAllGeofences(this)
-//        geofencing?.performPendingGeofenceTask("REMOVE")
-        // TODO remove item's marker
+        val batch = FirebaseFirestore.getInstance().batch()
+
+        // update mission state to Pause
+        val myMissionsRef = FirebaseFirestore.getInstance()
+                .collection("players").document(playerId)
+                .collection("myMissions").document(missionId)
+        batch.update(myMissionsRef, "state", "Paused")
+
+        // update the currentMissionId field in playerId document to null
+        val playerIdRef = FirebaseFirestore.getInstance()
+                .collection("players").document(playerId)
+        batch.update(playerIdRef, "currentMissionId", null)
+
+        batch.commit()
+                .addOnCompleteListener {
+                    Log.d(TAG, "UPDATE myMission state to Pause and UPDATE currentMissionId to null successful")
+                    isPlaying = false
+                    button_play.text = "Paused"
+
+                }.addOnFailureListener {
+                    Log.d(TAG, "UPDATE failed")
+                }
+
     }
 
 
